1ca155595170734cfb013e94defc4de90fd6d47c
Sumitjadav07 <sumitroyaltechnosoft7@gmail.com>
2024-10-22 23:31:28 +0530

    Did Changes in Files

--- src/main/java/com/controller/PaymentController.java
+++ src/main/java/com/controller/PaymentController.java
@@ -9,7 +9,6 @@ import com.repository.OrderRepository;
 import com.service.ChargeCreditCard;
 import com.service.CheckoutRequest;
 import com.service.PaymentResponse;
-import net.authorize.api.contract.v1.ANetApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 @RestController
@@ -35,48 +35,48 @@ public class PaymentController {
     private OrderRepository orderRepo;
 
     @PostMapping("/checkout")
-    public ResponseEntity<String> checkout(@RequestBody CheckoutRequest checkoutRequest){
-        // Handle Cash on delivery
+    public ResponseEntity<String> checkout(@RequestBody CheckoutRequest checkoutRequest) {
 
-        System.out.println("Checkout api starts");
-
-        if("COD".equalsIgnoreCase(checkoutRequest.getPaymentType())){
-            OrderEntity order = createOrder(checkoutRequest,null);
+        if ("COD".equalsIgnoreCase(checkoutRequest.getPaymentType())) {
+            OrderEntity order = createOrder(checkoutRequest, null);
             orderRepo.save(order);
             return ResponseEntity.ok("Order created successfully with Cash on delivery");
-        } else if("CreditCard".equalsIgnoreCase(checkoutRequest.getPaymentType())){
+        } else if ("CreditCard".equalsIgnoreCase(checkoutRequest.getPaymentType())) {
             PaymentResponse paymentResponse = processCreditCardPayment(checkoutRequest);
 
-            if(paymentResponse.isSuccess()){
+            if (paymentResponse.isSuccess()) {
                 OrderEntity order = createOrder(checkoutRequest, paymentResponse);
                 orderRepo.save(order);
                 return ResponseEntity.ok("Payment successful and Order created successfully");
             } else {
                 System.out.println("Payment failed: " + paymentResponse.getMessage());
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Payment failed: " + paymentResponse.getMessage());
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("Payment failed: " + paymentResponse.getMessage());
             }
         }
         return ResponseEntity.ok("Invalid payment method");
     }
 
-    private PaymentResponse processCreditCardPayment(CheckoutRequest checkoutRequest){
+    private PaymentResponse processCreditCardPayment(CheckoutRequest checkoutRequest) {
         return ChargeCreditCard.run(checkoutRequest);
     }
 
-    private OrderEntity createOrder(CheckoutRequest checkoutRequest, PaymentResponse paymentResponse){
+    private OrderEntity createOrder(CheckoutRequest checkoutRequest, PaymentResponse paymentResponse) {
         Optional<CustomerEntity> customerOp = customerRepo.findById(checkoutRequest.getCustomerId());
         CustomerEntity customer = customerOp.get();
         Optional<CartEntity> cartOp = cartRepo.findById(checkoutRequest.getCartId());
         CartEntity cart = cartOp.get();
+        
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm a dd-MM-yyyy");
 
         OrderEntity order = new OrderEntity();
         order.setCustomer(customer);
         order.setCart(cart);
         order.setTotalPaid(checkoutRequest.getTotalAmount());
-        order.setOrderDate(LocalDateTime.now().toString());
+        order.setOrderDate(LocalDateTime.now().format(formatter));
         order.setStatus(1);
 
-        if(paymentResponse != null){
+        if (paymentResponse != null) {
             order.setAuthCode(paymentResponse.getAuthCode());
             order.setTransactionId(paymentResponse.getTransactionId());
             order.setPaymentType("CreditCard");
@@ -85,5 +85,5 @@ public class PaymentController {
         }
         return order;
     }
-  
+
 }
